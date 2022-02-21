7ef29c97e27e857df0d92b5bea285c724881e5ae
Carlos Núñez <carlosnaguilera@gmail.com>
2022-02-21 11:02:00 +0100

    🎉 create dummy iOS project

---
+++ GitHubActionsPlayground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubActionsPlayground
+//
+//  Created by Carlos Núñez Aguilera on 21/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
